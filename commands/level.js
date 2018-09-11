b549bd0c5d5a50111da5981a46823e883ea234ab
ThisLightMan <>
2018-09-11 17:34:15 -0500

    Added level command

---
+++ commands/level.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const User = require("../models/user.js");
+module.exports.run = async (bot, message, args) => {
+    let user = await User.findorCreate(message.guild, message.author);
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setTimestamp()
+    .setFooter((user.level*500)-user.xp + " xp needed to level up!")
+    .addField("XP", user.xp, true)
+    .addField("Level", user.level, true)
+    .setColor("#2159b2");
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "level"
+}
