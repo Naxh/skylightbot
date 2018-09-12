fab80a19daf838b0a665c9aa67dc72f54ca36c93
ThisLightMan <>
2018-09-12 17:06:34 -0500

    Fixed coins command

--- commands/coins.js
+++ commands/coins.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const User = require("../models/user.js");
-
 module.exports.run = async (bot, message, args) => {
     let user = await User.findOrCreate(message.guild, message.author);
     let embed = new Discord.RichEmbed()
@@ -9,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Coin Multiplier", user.coinmultiplier)
     .setTimestamp()
     .setColor("#2159b2");
+    message.channel.send(embed);
 }
 module.exports.help = {
     name: "coins"
