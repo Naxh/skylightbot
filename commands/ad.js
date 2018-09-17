650df6442fee0a4a899745ace73abdaea669da20
ThisLightMan <birdkage56@gmail.com>
2018-09-16 19:30:03 -0500

    Testing

--- commands/ad.js
+++ commands/ad.js
@@ -1,10 +1,10 @@
 module.exports.run = async (bot, message, args) => {
     if(!args[0] || !args[1]) return message.channel.send("Invalid format! ``-ad <ad number> <content>``");
-    let msg;
-    await message.guild.channels.get("491032995646668820").fetchMessages(msgs => {msgs.array().forEach(element => {if(element.content.split("\n")[0].split("Advertisement ")[0] == args[0]) msg = element})});
-    if(!msg) return message.channel.send("I could not find that advertisement!");
-    if(msg.content.split("\n")[1].split("Owner: ")[0] !== message.author.id) return message.channel.send("You are not the owner of that advertisement!");
-    
+    message.guild.channels.get("491032995646668820").fetchMessages(msgs => {msgs.array().forEach(msg => {if(msg.content.split("\n")[0].split("Advertisement ")[0] == args[0]) {
+        if(msg.content.split("\n")[1].split("Owner: ")[0] !== message.author.id) return message.channel.send("You are not the owner of that advertisement!");
+        message.reply("test")
+    }})});
+
 }
 module.exports.help = {
     name: "ad"
